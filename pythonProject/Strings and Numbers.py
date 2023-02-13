916b2b4d6e2f1d578eac2a03b55518b8396ef0e3
Adriaan Cronjé <piecro060620@student.jenseneducation.se>
2023-02-13 11:57:51 +0100

    these are my files

---
+++ pythonProject/Strings and Numbers.py
@@ -0,0 +1,30 @@
+
+import math
+
+
+num1 = input("enter a number")
+num2 = input("enter another number")
+num3 = input("and yet another number")
+num4 = input("another")
+num5 = input("another")
+num6 = input("one last")
+result = int(num1) / int(num2), int(num3) / int(num4), int(num5) / int(num6)
+print(result)
+
+
+class Fractions:
+    
+
+
+
+Firstname = input("what is your name?")
+Surname = input("what is your surname")
+print(Firstname[:4]+Surname[-3:])
+
+
+
+
+
+
+
+
