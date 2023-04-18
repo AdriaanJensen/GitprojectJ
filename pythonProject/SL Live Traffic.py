021b3d89ae5b7b9cb0fd2a22266a5fc516c668a6
Adriaan Cronjé <piecro060620@student.jenseneducation.se>
2023-04-18 14:41:48 +0200

    Please work

--- pythonProject/SL Live Traffic.py
+++ pythonProject/SL Live Traffic.py
@@ -1,44 +1,107 @@
 import json
 import requests
 import pprint
+from datetime import datetime
+from datetime import date
+
+date_today = date.today()
+print("Today's date:", date_today)
+time_now = datetime.now().strftime("%H:%M:%S")
+print("the current time is", time_now)
+
+url1 = 'https://api.sl.se/api2/typeahead.json'
+parametrar1 = {
+    'key': 'YOUR_KEY_HERE',
+    #'searchstring': 'Däckelvägen', #namnet på platsen eller stationen/hållplatsen som ska skannas
+    'searchstring': 'Jakobsberg_station', #namnet på platsen eller stationen/hållplatsen som ska skannas
+    'stationsonly': 'True', #Lägg falsk om du vill kunna se hållplatser och addresser
+}
+Svar1 = requests.get(url1, parametrar1) #frågan efter API informationen
+Info1 = Svar1.json() #Få datan som Json för att kunna läsa
+resultat1 = Info1['ResponseData'][0] #Få det första resultatet på datan
+originId = resultat1['SiteId'] #Faktiskt få Id till hållplatsen
+print(originId) #print
 
 
-#url = 'https://api.sl.se/api2/LineData.json?model=[https://api.sl.se/api2/LineData]&key=[KEY]'
-#r = requests.get(url)
+url2 = 'https://api.sl.se/api2/typeahead.json'
+parametrar2 = {
+    'key': 'YOUR_KEY_HERE',
+    'searchstring': 'Fridhemsplan', #namnet på platsen eller stationen/hållplatsen som ska skannas
+    'stationsonly': 'True', #Lägg falsk om du vill kunna se hållplatser
+}
+Svar2 = requests.get(url1, parametrar2) #frågan efter API informationen
+Info2 = Svar2.json() #Få datan som Json för att kunna läsa
+resultat2 = Info2['ResponseData'][0] #Få det första resultatet på datan
+destId = resultat2['SiteId'] #Faktiskt få Id till hållplatsen
+print(destId) #print
 
 
-#Trip = 'https://api.sl.se/api2/TravelplannerV3_1/trip.<json>?key=<KEY>¶metrar'
-#Trip = 'https://api.sl.se/api2/TravelplannerV3_1'
 
-TestApi = "https://api.tvmaze.com/singlesearch/shows"
-Params = {"q":"girls"}
-response1 = requests.get(TestApi, Params)
+Homeyurl = "https://webhooks.athom.com/webhook/57274085acb3bd6d24b3d200/?token=irs8iS044k"
+Data = {
+    "event": "Traffic",
+    "data1": str(date_today),
+    "data2": originId,
+    "data3": destId
+}
+requests.post(Homeyurl, data=json.dumps(Data), headers={"Content-Type": "application/json"})
 
-if response1.status_code == 200:
-    data = json.loads(response1.text)
-    pprint.pprint(data)
+
+Traffic = 'https://api.sl.se/api2/deviations.json'
+params = {
+    #"siteId": 9260,
+    "siteId": 9702,
+    "key": 'YOUR_KEY_HERE',
+    "transportMode": 'train, bus'
+}
+svar1 = requests.get(Traffic, params) #Calling Data from Trafiklabs
+direktiv1 = svar1.json() #Turning Data into Json format
+print(direktiv1) #checking Data returned
+if "StatusCode" in direktiv1 and direktiv1["StatusCode"] != 0: #if not 0 there are problems
+    storning1 = direktiv1["ResponseData"]["TrafficTypes"][0]["Deviations"]
+    for storning1 in storning1:
+        print(f"Type: {storning1['Scope']}, Message: {storning1['Message']}")
 else:
-    print(f"Error: {response1.status_code}")
-
-word = input("what word would you like to translate?")
-url = 'https://christianthompson.com/dictionary.py?'
-
-params = {"word":word}
-response = requests.get(url, params)
-
-print(f"Status Code: {response.status_code}")
-
-print(response.text)
+    print("Inga störningar hittade")
 
 
 
+Resa = 'https://api.sl.se/api2/TravelplannerV3_1/trip.<json>'
 
+Params = {
+    'key': 'YOUR_KEY_HERE',
+    'originId': '5784',
+    'destId': '9115',
+    'searchForArrival': '0',
+    'time': '2023-04-15T09:30:00',
+    #'time': date_today+time_now,
+    'products': 'BUS,TRAIN,METRO,TRAM,SHIP',
+}
+print(Params)
 
+Response = requests.get(Resa, Params)
+print(Response)
 
+if Response.status_code == 200:
+    data = Response.json()
+    if 'Trip' in data and data['Trip']:
+        result = data['Trip'][0]
+        departure_time = result['LegList']['Leg'][0]['Origin']['time']
+        arrival_time = result['LegList']['Leg'][-1]['Destination']['time']
+        print(f"Departure time: {departure_time}")
+        print(f"Arrival time: {arrival_time}")
+    else:
+        print("No results found.")
+else:
+    print(f"Error {Response.status_code}: {Response.reason}")
 
-
-
-
+Information = Response.json()
+print(Information)
+Result = Information['Trip'][0]
+avGangsTid = Result['Leglist']['Leg'][0]['Origin']['Tid']
+anKomstTid = Result['Leglist']['Leg'][-1]['Destination']['Tid']
+print(f"Departute time: {avGangsTid}")
+print(f"Arrival time: {anKomstTid}")
 
 
 
--- pythonProject/webhook.py
+++ pythonProject/webhook.py
@@ -1,5 +1,15 @@
 import requests
 import json
-webhook_url = "https://webhook.site/014ede3f-3bb0-4991-b596-c26f22c87d7a"
+webhook_url = "https://webhook.site/d8c79cfd-d149-4ec4-8cdb-967fcfa05ad4"
 data = {" name" : "Lukas"}
 requests.post(webhook_url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+Homeyurl = "https://webhooks.athom.com/webhook/57274085acb3bd6d24b3d200/?token=irs8iS044k"
+Data = {
+    "event": "Traffic",
+    "data1": "Train",
+    "data2": "5794",
+    "data3": "9114"
+}
+
+requests.post(Homeyurl, data=json.dumps(Data), headers={"Content-Type": "application/json"})
