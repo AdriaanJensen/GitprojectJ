19287e9700ec64f6a8c1db0e6f2a2f56c200c956
AdriaanJensen <125407557+AdriaanJensen@users.noreply.github.com>
2023-04-18 15:03:50 +0200

    Delete APIs.py

--- pythonProject/APIs.py
+++
@@ -1,8 +0,0 @@
-
-Route-planner = 6db6ac5b7d684cfe950a76afe4009d79
-
-Location = 0d758be71cf1468492dc46b20570884d
-
-Stations-lines = 8aaf0dc63aa34b159bbe435482af54b1
-
-Traffic-Distubance = 1709e26076a44936a8f82f677745ec13
